efe44b3c20b2a01c0c1767c00d82193ab73317c2
Oguzhan Ertas <74022564+oerts@users.noreply.github.com>
2024-05-19 05:18:57 -0400

    feat: update personal information

--- src/persona.js
+++ src/persona.js
@@ -1,54 +1,40 @@
 export const persona = {
-    firstName: "Jack",
-    secondName: "Frost",
-    description: "Independent craftsman, spirited man, mechanic",
-    email: "luckybastard@skiff.com",
+    firstName: "Oguzhan",
+    secondName: "Ertas",
+    description: "Bender of Cascading Style Sheets, research addict",
+    email: "howdy@ertas.dev",
     story:
-      "Ad pariatur ad do incididunt laborum laboris ullamco voluptate consequat qui sint. Cillum nisi eu excepteur non ipsum qui commodo et veniam sunt. Minim quis eu reprehenderit irure nulla sit irure. Labore eu enim commodo enim eiusmod Lorem deserunt eiusmod ex eu. Dolor tempor nisi id voluptate minim. Sit occaecat eiusmod cillum et aliquip.",
+      "Hello! I'm Oz. Passionate UI developer driven by curiosity and a thirst for learning.",
   sections: {
     // You can create your own sections. But! For reducing headache in future, change the values (content), but don't change the keys ('title', 'desc').
-    books: [
+    talks: [
         {
-            title: "Shop Class as Soulcraft",
-            desc: 'Matthew B. Crawford',
-            url: 'https://www.amazon.com/Shop-Class-Soulcraft-Inquiry-Value/dp/0143117467?keywords=soulcraft&qid=1700379052&sr=8-3'
-        },
-        {
-            title: "Why We Drive",
-            desc: 'Matthew B. Crawford',
-            url: 'https://www.amazon.com/Why-We-Drive-Toward-Philosophy/dp/0062741977?crid=2Z7ID7AXG1M8P&keywords=why+we+drive&qid=1700379191&sprefix=why+we+dri,aps,410&sr=8-1'
-        },
-        {
-            title: "The Tinkering Mind",
-            desc: 'Tillmann Vierkant',
-            url: 'https://www.amazon.com/Tinkering-Mind-Agency-Cognition-Extended-ebook/dp/B0BDZZGJRS?crid=2EJ2CHS3HAB8F&keywords=tinkering+mind&qid=1700379284&s=books&sprefix=tinkering+mi,stripbooks-intl-ship,296&sr=1-1'
+            title: "Navigating Redesigns",
+            desc: 'BBT',
+            url: 'https://bursa.dev/event-detail/bbt-connect-atolye15-e0c52b3a'
         },
     ],
     experience: [
         {
-            title: "Indian Motorcycles",
-            desc: '2010-now',
-            url: 'https://www.indianmotorcycle.com/en-us/'
-        },
-        {
-            title: "Harley Davidson",
-            desc: '1999-2010',
-            url: 'https://www.harley-davidson.com/us/en/index.html'
+            title: "Atolye15",
+            desc: '2022-now',
+            url: 'https://www.atolye15.com/'
         },
     ],
   },
       contacts: [
+        {
+          title: "linkedin",
+          url: "https://linkedin.com/in/ertaso",
+        },
       {
-        title: "x",
-        url: "https://twitter.com/0x_shapeshifter",
+        title: "github",
+        url: "https://github.com/oerts",
       },
       {
-        title: "mastodon",
-        url: "https://mastodon.social/@0xshapeshifter",
+        title: "x",
+        url: "https://twitter.com/ertsoz",
       },
     ],
 
   };
-
-    // Don't forget to save upon changing the contents of the file ;)
-
